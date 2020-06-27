f7c33fe6ef437195dc8dff8c63b730c168f66640
jadiscke <vjtasso@gmail.coom>
2020-06-27 11:43:03 -0300

    Create instructors methods and refactor routes

---
+++ src/instructors.js
@@ -0,0 +1,14 @@
+// create
+exports.post = function(req,res){
+  const keys = Object.keys(req.body);
+
+  for (const key of keys) {
+    if (req.body[key] == ""){
+      return res.send('Please, fill all fields');
+    }
+  }
+  return res.send(req.body);
+}
+// update
+
+// delete
