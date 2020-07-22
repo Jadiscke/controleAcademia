bf073236bd54b1f59246469c98d92af3ee62191d
jadiscke <vjtasso@gmail.coom>
2020-07-22 10:56:42 -0300

    Change Utils export format

--- src/app/lib/utils.js
+++ src/app/lib/utils.js
@@ -1,10 +1,10 @@
 module.exports ={
-  age: function(timestamp){
+  age(timestamp){
     const milisecondsYear = Date.parse(1971,1,1);
     return parseInt((Date.now() - timestamp)/milisecondsYear,10)
   },
 
-  date: function(timestamp){
+  date(timestamp){
     // return date to HTML format
     return new Date(timestamp).toISOString().slice(0,10);
   }
