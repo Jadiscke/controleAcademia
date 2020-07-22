bc85fc7fa9dfc6ce7a434703d382478531073bb6
jadiscke <vjtasso@gmail.coom>
2020-07-22 19:38:49 -0300

    Modify Controller to accept Model

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,50 +1,50 @@
-const { formatInstructors, formatInstructor } = require('../lib/utils');
-const Instructor = require('../models/instructor');
-Intl = require('intl');
+const { formatInstructors, formatInstructor, date } = require("../lib/utils");
+const Instructor = require("../models/instructor");
 
 module.exports = {
-  index(req,res){
-
-    Instructor.all(function(instructors) {
+  index(req, res) {
+    Instructor.all(function (instructors) {
       formatInstructors(instructors);
-      return res.render('instructors/index', {instructors});
-    })
-
-    
+      return res.render("instructors/index", { instructors });
+    });
   },
-  create(req,res){
-    return res.render('instructors/create');
+  create(req, res) {
+    return res.render("instructors/create");
   },
-  post(req,res){
+  post(req, res) {
     const keys = Object.keys(req.body);
 
     for (const key of keys) {
-      if (req.body[key] == ""){
-        return res.send('Please, fill all fields');
+      if (req.body[key] == "") {
+        return res.send("Please, fill all fields");
       }
     }
 
-    Instructor.create(req.body,function(instructor){
+    Instructor.create(req.body, function (instructor) {
       return res.redirect(`instructors/${instructor.id}`);
     });
-    
-    
-    
-    },
-  show(req,res){
-    Instructor.find(req.params.id,function(instructor){
+  },
+  show(req, res) {
+    Instructor.find(req.params.id, function (instructor) {
       formatInstructor(instructor);
-      return res.render('instructors/show',{instructor});
-    })
-    },
-  edit(req,res){
-    
-  return
+      return res.render("instructors/show", { instructor });
+    });
   },
-  put(req,res){
+  edit(req, res) {
+    Instructor.find(req.params.id, function (instructor) {
+      instructor.birth = date(instructor.birth).iso;
+      return res.render("instructors/edit", { instructor });
+    });
+
+    return;
+  },
+  put(req, res) {
+    Instructor.update(req.body,function(){
+      return res.redirect(`instructors/${req.body.id}`);
+    });
     return
   },
-  delete(req,res){
-    return
+  delete(req, res) {
+    return;
   },
-}
+};
